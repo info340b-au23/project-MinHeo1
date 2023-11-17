7aff3d748e58b3a91d17dedf705f31f240fa0806
Jinsu-J <jjs990422@gmail.com>
2023-11-17 00:31:39 -0800

    dropbox

---
+++ src/dropdown.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Dropdown({ options, onSelect, label }) {
+  return (
+    <div className="dropdown">
+      <label>{label}</label>
+      <select onChange={(e) => onSelect(e.target.value)}>
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
