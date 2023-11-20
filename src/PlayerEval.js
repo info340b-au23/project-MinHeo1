387a74858e205ffee96bf62ba77bbf6f491b7510
OscarMaldonado <oscarj23@uw.edu>
2023-11-20 15:11:34 -0800

    fantasy data in json format

---
+++ src/PlayerEval.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Dropdown from "./dropdown";
+export function Evaluation(props) { //pass in data of all the players as a prop?
+    return (
+        <>
+            <h2>Compare Players: </h2>
+            
+        </>
+    )
+}
